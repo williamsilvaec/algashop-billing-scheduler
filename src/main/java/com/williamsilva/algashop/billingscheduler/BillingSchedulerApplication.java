589b2943893de96f0a96b1b3fd183c581026356c
William Silva <williamsilvaec@gmail.com>
2026-01-27 18:11:27 -0300

    chore: adicionar microserviço billing-scheduler

---
+++ src/main/java/com/williamsilva/algashop/billingscheduler/BillingSchedulerApplication.java
@@ -0,0 +1,13 @@
+package com.williamsilva.algashop.billingscheduler;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BillingSchedulerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BillingSchedulerApplication.class, args);
+	}
+
+}
